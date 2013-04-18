c572ff75190b0eb6260cfaedf5e72f86a004ed87
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-04-19 01:06:44 +0200

    Add menu

--- script/script.js
+++ script/script.js
@@ -22,6 +22,14 @@ workshop.controller("HelloCtrl", function($scope) {
 
 workshop.controller("MenuCtrl", function($scope) {
   // TODO add a list of object with menu possition data
-  $scope.href = "http://google.pl";
-  $scope.text = "go to google";
+  $scope.links = [
+    {
+      url: '#/main',
+      text: 'Main'
+    },
+    {
+      url: '#/hello',
+      text: 'Hello'
+    }
+  ];
 });
