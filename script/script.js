075a951e47d0095eb1e969318afe46de9525b2fe
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-04-19 00:18:04 +0200

    Describe todo-1

--- script/script.js
+++ script/script.js
@@ -4,3 +4,5 @@ var workshop = angular.module("workshop", []);
 workshop.controller("HelloCtrl", function($scope) {
   $scope.helloMessage = "Hello world";
 });
+
+// TODO-1 add MenuCtrl, and define data necessary to create a link
