9f43f46e43f38b465e6e184202d8a33a4b1cef16
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-04-19 00:30:57 +0200

    Define routes

--- script/script.js
+++ script/script.js
@@ -1,6 +1,21 @@
 // Create a new module
 var workshop = angular.module("workshop", []);
 
+// Create routes
+workshop.config(function($routeProvider) {
+  $routeProvider
+    .when("/hello", {
+      templateUrl: "views/hello.html",
+      controller: "HelloCtrl"
+    })
+    .when('/main', {
+      templateUrl: 'views/main.html'
+    })
+    .otherwise({
+      redirectTo: '/main'
+    });
+});
+
 workshop.controller("HelloCtrl", function($scope) {
   $scope.helloMessage = "Hello world";
 });
